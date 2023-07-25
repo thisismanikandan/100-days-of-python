5b877e9577246404f513fb499b371a3ad6152461
Mani <manipatta747@gmail.com>
2023-07-25 08:27:45 +0530

    Adding day 6 files

---
+++ day-6-codingroom.py
@@ -0,0 +1,15 @@
+# Maze
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while at_goal() != True:
+#     while front_is_clear() and wall_on_right():
+#         move()
+#     while at_goal() != True:
+#         if right_is_clear():
+#             turn_right()
+#             move()
+#         else:
+#             turn_left()
