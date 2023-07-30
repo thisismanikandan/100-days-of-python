740d8ddff4af006b67b2c3f2b24bc47994d2fae9
Mani <manipatta747@gmail.com>
2023-07-31 00:45:24 +0530

    Made some changes

--- day-10-codingroom.py
+++ day-10-codingroom.py
@@ -1,31 +1,31 @@
-# # Leap year with function
-# def is_leap(year):
-#   """Checks if the year is a leap year or not"""
-#   if year % 4 == 0:
-#     if year % 100 == 0:
-#       if year % 400 == 0:
-#         return True
-#       else:
-#         return False
-#     else:
-#       return True
-#   else:
-#     return False
+# Leap year with function
+def is_leap(year):
+  """Checks if the year is a leap year or not"""
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
 
-# def days_in_month(year, month):
-#   """Checks if the year is leap and if month is 2 if so return 29"""
-#   month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-#   if is_leap(year):
-#       if month == 2:
-#         return 29
-#   return month_days[month-1]
+def days_in_month(year, month):
+  """Checks if the year is leap and if month is 2 if so return 29"""
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  if is_leap(year):
+      if month == 2:
+        return 29
+  return month_days[month-1]
 
   
-# #🚨 Do NOT change any of the code below 
-# year = int(input("Enter a year: "))
-# month = int(input("Enter a month: "))
-# days = days_in_month(year, month)
-# print(days)
+#🚨 Do NOT change any of the code below 
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
+days = days_in_month(year, month)
+print(days)
 
 # ----------
 # Calculator
