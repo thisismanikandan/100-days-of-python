2f82c551646b9904e3fccd2b387fc798de0ebd90
Mani <manipatta747@gmail.com>
2023-08-13 04:28:46 +0530

    made some changes

--- coffeemachine.py
+++ coffeemachine.py
@@ -7,8 +7,30 @@ resources = {
     'money' : 0
 }
 
+needed_resources = {
+    "espresso": {
+        "water": 50,
+        "coffee": 18,
+    },
+    "latte" : {
+        "water": 200,
+        "coffee": 24,
+        "milk": 150,
+    },
+    "cappucino" : {
+        "water": 250,
+        "coffee": 24,
+        "milk": 100,
+    }
+}
+
+
 while game_is_on:
+
     choice = input("\nWhat would you like? (espresso/ latte/ cappucino): ")
+
+    if choice == "off":
+        game_is_on = False
 
     def update_resource(choice):
         if choice == "espresso":
@@ -24,26 +46,6 @@ while game_is_on:
             resources["water"] -= 250
             resources["coffee"] -= 24
             resources["milk"] -= 100
-
-    # # coins
-    # penny = 1 cent = $0.01
-    # nickel = 5 cents = $0.05
-    # dime = 10 cents = $0.1
-    # quarter = 25 cents = $0.25
-
-    # # espresso $1.50
-    # 50ml water
-    # 18g coffee
-
-    # # latte $2.50
-    # 200ml water
-    # 24g coffee
-    # 150ml milk
-
-    # # cappucino $3.00
-    # 250ml water
-    # 24g coffee
-    # 100ml milk
 
     def check_balance(total):
         if choice == "espresso":
@@ -78,7 +80,7 @@ while game_is_on:
                 process_coins()
         
 
-    def process_coins():
+    def process_coins():   
         quarters = int(input("How many quarters?: "))
         dimes = int(input("How many dimes?: "))
         nickles = int(input("How many nickles?: "))
@@ -89,49 +91,60 @@ while game_is_on:
 
 
     def check_resource(choice):
-        if choice == "espresso":
-            if resources["water"] > 50:
-                if resources["coffee"] > 18:
-                    print("\nEnter coins\n ")
-                    process_coins()
-                    print("Here is your espresso, Enjoy !!!")
-                    update_resource(choice)
-                    print(resources)
-                else:
-                    print("\nSorry, there is no enough coffee")
-            else:
-                print("\nSorry, there is no enough water")
-        if choice == "latte":
-            if resources["water"] > 200:
-                if resources["coffee"] > 24:
-                    if resources["milk"] > 150:
-                        print("\nEnter coins\n ")
-                        process_coins()
-                        print("Here is your latte, Enjoy !!!")
-                        update_resource(choice)
-                        print(resources)
-                    else:
-                        print("\nSorry, there is no enough milk")
-                else:
-                    print("\nSorry, there is no enough coffee")
-            else:
-                print("\nSorry, there is no enough water")
-        if choice == "cappucino":
-            if resources["water"] > 250:
-                if resources["coffee"] > 24:
-                    if resources["milk"] > 100:
-                        print("\nEnter coins\n ")
-                        process_coins()
-                        print("Here is your cappucino, Enjoy !!!")
-                        update_resource(choice)
-                        print(resources)
-                    else:
-                        print("\nSorry, there is no enough milk")
-                else:
-                    print("\nSorry, there is no enough coffee")
-            else:
-                print("\nSorry, there is no enough water")
+        for each in needed_resources[choice]:
+            resources[each] -= needed_resources[choice][each]
+        if resources[each] == 0:
+            print("\nSorry, there is no enough water")
+        else:
+            print("\nEnter coins\n ")
+            process_coins()
+            print(f"Here is your {choice}, Enjoy !!!")
+            update_resource(choice)
+            print(resources)
+    #     if choice == "espresso":
+    #         if resources["water"] > 50:
+    #             if resources["coffee"] > 18:
+    #                 print("\nEnter coins\n ")
+    #                 process_coins()
+    #                 print("Here is your espresso, Enjoy !!!")
+    #                 update_resource(choice)
+    #                 print(resources)
+    #             else:
+    #                 print("\nSorry, there is no enough coffee")
+    #         else:
+    #             print("\nSorry, there is no enough water")
+    #     if choice == "latte":
+    #         if resources["water"] > 200:
+    #             if resources["coffee"] > 24:
+    #                 if resources["milk"] > 150:
+    #                     print("\nEnter coins\n ")
+    #                     process_coins()
+    #                     print("Here is your latte, Enjoy !!!")
+    #                     update_resource(choice)
+    #                     print(resources)
+    #                 else:
+    #                     print("\nSorry, there is no enough milk")
+    #             else:
+    #                 print("\nSorry, there is no enough coffee")
+    #         else:
+    #             print("\nSorry, there is no enough water")
+    #     if choice == "cappucino":
+    #         if resources["water"] > 250:
+    #             if resources["coffee"] > 24:
+    #                 if resources["milk"] > 100:
+    #                     print("\nEnter coins\n ")
+    #                     process_coins()
+    #                     print("Here is your cappucino, Enjoy !!!")
+    #                     update_resource(choice)
+    #                     print(resources)
+    #                 else:
+    #                     print("\nSorry, there is no enough milk")
+    #             else:
+    #                 print("\nSorry, there is no enough coffee")
+    #         else:
+    #             print("\nSorry, there is no enough water")
 
+    
     if choice == "report":
         print(resources)
     else:
